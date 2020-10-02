7bf7aaf21f52ba15975579bc54c8aa95b629924a
pontakornth <pontakorn61@gmail.com>
2020-10-02 15:09:26 +0700

    feat: Add bare login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <form class="login-form">
+      <h1>Login</h1>
+      <label for="email">Email</label>
+      <input name="email" id="email" type="email" placeholder="example@mail.com" :model="email" />
+      <label for="password">Password</label>
+      <input name="password" id="password" type="password" :model="password" />
+  </form>
+</template>
+
+<script lang="ts">
+// This is for practicing composition API
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const email = ref();
+    const password = ref();
+
+    return {
+      email,
+      password,
+    };
+  },
+};
+</script>
---
+++ src/pages/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login">
+      <LoginForm />
+  </div>
+</template>
+
+<script lang="ts">
+import LoginForm from '../components/LoginForm.vue';
+
+export default {
+  components: {
+    LoginForm,
+  },
+};
+</script>
