f8c5572b2e65da520f2c7fb7918f1d5538fcbdde
pontakornth <pontakorn61@gmail.com>
2020-10-02 15:11:56 +0700

    refactor: Move About page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue';
+import Login from '../pages/login.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: Login,
   },
   {
     path: '/about',
@@ -13,7 +13,7 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '../pages/About.vue'),
   },
 ];
 
