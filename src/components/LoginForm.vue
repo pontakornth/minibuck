f36e6d028518c9776397a819ac3e215d5fb84bd0
pontakornth <pontakorn61@gmail.com>
2020-10-02 15:48:52 +0700

    feat: Add login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -2,24 +2,31 @@
   <form class="login-form">
       <h1>Login</h1>
       <label for="email">Email</label>
-      <input name="email" id="email" type="email" placeholder="example@mail.com" :model="email" />
+      <input name="email" id="email" type="email" :value="email" @input="updateEmail"/>
       <label for="password">Password</label>
-      <input name="password" id="password" type="password" :model="password" />
+      <input name="password" id="password" type="password" :value="password"
+          @input="updatePassword" />
   </form>
 </template>
 
 <script lang="ts">
 // This is for practicing composition API
-import { ref } from 'vue';
-
+// TODO: Update the type to actual type
 export default {
-  setup() {
-    const email = ref();
-    const password = ref();
-
+  props: {
+    email: String,
+    password: String,
+  },
+  setup(props: any, { emit }: any) {
+    const updateEmail = (e: any) => {
+      emit('update:email', e.target.value);
+    };
+    const updatePassword = (e: any) => {
+      emit('update:password', e.target.value);
+    };
     return {
-      email,
-      password,
+      updateEmail,
+      updatePassword,
     };
   },
 };
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="login">
-      <LoginForm />
+      <LoginForm v-model:email="email" v-model:password="password" />
   </div>
 </template>
 
 <script lang="ts">
+import { ref } from 'vue';
 import LoginForm from '../components/LoginForm.vue';
 
 export default {
+  setup() {
+    const email = ref('');
+    const password = ref('');
+    return {
+      email,
+      password,
+    };
+  },
   components: {
     LoginForm,
   },
