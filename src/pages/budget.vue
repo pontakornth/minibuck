d3a2e54b73806a0836b32abf9d753189cce0ec56
pontakornth <pontakorn61@gmail.com>
2020-10-03 15:11:25 +0700

    feat: Add budget page

---
+++ src/pages/budget.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="budget">
+    <h1>Budget</h1>
+    <ul class="item-list">
+      <li class="item" v-for="item in items" :key="item">
+        <h2>Item</h2>
+        <ul>
+          <li>Price: xxx</li>
+          <li>Saved: xxx</li>
+          <li>Left: xxx</li>
+        </ul>
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const items = ref([1, 2, 3, 4, 5]);
+    return {
+      items,
+    };
+  },
+});
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -15,6 +15,11 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../pages/About.vue'),
   },
+  {
+    path: '/budget',
+    name: 'Budget',
+    component: () => import(/* webpackChunkName: budget */ '../pages/budget.vue'),
+  },
 ];
 
 const router = createRouter({
