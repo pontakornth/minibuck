2cfbe360a49f39bb73db7bd9dc9634dce63ee0ca
pontakornth <pontakorn61@gmail.com>
2020-10-03 17:43:32 +0700

    styling: style for budget page

--- src/pages/budget.vue
+++ src/pages/budget.vue
@@ -3,8 +3,8 @@
     <h1>Budget</h1>
     <ul class="item-list">
       <li class="item" v-for="item in items" :key="item">
-        <h2>Item</h2>
-        <ul>
+        <h2 class="item-name">Item</h2>
+        <ul class="item-properties">
           <li>Price: xxx</li>
           <li>Saved: xxx</li>
           <li>Left: xxx</li>
@@ -12,6 +12,9 @@
 
       </li>
     </ul>
+    <div class="menu">
+      <button class="menu-button">Add item</button>
+    </div>
   </div>
 </template>
 
@@ -27,3 +30,24 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="stylus">
+.item-list
+  list-style none
+  display grid
+  grid-template-columns repeat(3, 1fr)
+  grid-gap 16px
+  padding 1rem
+.item
+  border: 1px solid lighten(#000, 30%)
+  padding 1rem
+.item-properties
+  list-style none
+  text-align right
+.menu-button
+  border 1px solid lighten(rgba(0,0,0,.75), 30%)
+  padding 0.333em
+  font-size 1.3rem
+  background red
+  color white
+</style>
