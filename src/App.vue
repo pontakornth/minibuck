665e4fc2d9d2aedfdea9f7c6ccf1fccfc3a09a65
pontakornth <pontakorn61@gmail.com>
2020-10-04 09:42:38 +0700

    styling: Include base style and font

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@ html, body, * {
   box-sizing border-box
 }
 #app
-  font-family Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
   text-align center
